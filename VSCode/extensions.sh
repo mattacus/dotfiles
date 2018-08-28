b6a524a7d198c2f746d10e4d460e129671d153a0
mattl <matt.lee.personal@gmail.com>
2018-08-27 23:23:54 -0500

    add vscode settings

---
+++ VSCode/extensions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension bierner.color-info
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension formulahendry.auto-close-tag
+code --install-extension karyfoundation.comment
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension PKief.material-icon-theme
+code --install-extension techer.open-in-browser
+code --install-extension twxs.cmake
+code --install-extension vector-of-bool.cmake-tools
+code --install-extension WallabyJs.quokka-vscode
+code --install-extension wmaurer.change-case
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension xabikos.ReactSnippets
