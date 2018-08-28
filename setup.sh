1dfe1abb6edf830ef9c8e6479edf2eeecffc0dfe
mattl <matt.lee.personal@gmail.com>
2018-08-27 23:01:52 -0500

    add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Create symbolic links for dotfiles
+ln -sv ~/.dotfiles/.bash_profile ~
+ln -sv ~/.dotfiles/.dir_colors ~
+ln -sv ~/.dotfiles/.gitconfig ~
+ln -sv ~/.dotfiles/.gitignore_global ~
