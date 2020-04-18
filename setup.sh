fa19b73271215969911f6e5d929fab03e1cce0e3
mattl <matt.lee.personal@gmail.com>
2020-04-18 11:35:06 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@
 ln -sv ~/.dotfiles/.bash_profile ~
 ln -sv ~/.dotfiles/.dir_colors ~
 ln -sv ~/.dotfiles/.gitconfig ~
+ln -sv ~/.dotfiles/.git-completion.bash ~
+ln -sv ~/.dotfiles/.git-prompt.sh ~
 ln -sv ~/.dotfiles/.gitignore_global ~
 ln -sv ~/.dotfiles/.vimrc ~
